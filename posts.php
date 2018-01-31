67bde18f9848e63fd6ed8e079d148ba723081823
Tamara Vlaovic <vlaovic.tamara@gmail.com>
2018-01-31 21:58:18 +0100

    zadatak 4

--- posts.php
+++ posts.php
@@ -1,3 +1,51 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "vivify";
+$dbname = "blog";
+try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+
+    $sql = "SELECT Id, Title, Body, Author, Created_at FROM posts ORDER BY Created_at DESC";
+                $statement = $connection->prepare($sql);
+                // izvrsavamo upit
+                $statement->execute();
+                // zelimo da se rezultat vrati kao asocijativni niz.
+                // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+                $statement->setFetchMode(PDO::FETCH_ASSOC);
+                // punimo promenjivu sa rezultatom upita
+                $posts = $statement->fetchAll();
+                // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+                    // echo '<pre>';
+                    // var_dump($posts);
+                    // echo '</pre>';
+
+?>
+
+
+           <?php
+                foreach ($posts as $post) {
+            ?>
+             <div class="blog-post">
+
+               <h2 class="blog-post-title"><a href="/partials/single-post1.php"><?php echo($post['Title']) ?></a></h2>
+                <p class="blog-post-meta"><?php echo($post['Created_at']) ?> by <a href="#"><?php echo($post['Author']) ?></a></p>
+
+               <p><?php echo($post['Body']) ?></p>
+  </div><!-- /.blog-post -->
+
+
+           <?php
+                }
+            ?>
+
 <?php include 'header.php';?>
 <div class="col-sm-8 blog-main">
 
